54456f98fba2a9760416ff6a8dc110f0398fa95c
Altamash Ali <altamashaali@gmail.com>
2025-08-19 13:54:21 -0500

    add webasm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys, random
+import pygame, sys, random, asyncio
 from pygame.math import Vector2
 
 pygame.init()
@@ -113,35 +113,39 @@ food_surface = pygame.image.load("Graphics/food.png")
 SNAKE_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SNAKE_UPDATE, 200) 	
 
-while True:
-	for event in pygame.event.get():
-		if event.type == SNAKE_UPDATE:
-			game.update()
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
+async def main():
+	while True:
+		for event in pygame.event.get():
+			if event.type == SNAKE_UPDATE:
+				game.update()
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
 
-		if event.type == pygame.KEYDOWN:
-			if game.state == "STOPPED":
-				game.state = "RUNNING"
-			if event.key == pygame.K_UP and game.snake.direction != Vector2(0, 1):
-				game.snake.direction = Vector2(0, -1)
-			if event.key == pygame.K_DOWN and game.snake.direction != Vector2(0, -1):
-				game.snake.direction = Vector2(0, 1)
-			if event.key == pygame.K_LEFT and game.snake.direction != Vector2(1, 0):
-				game.snake.direction = Vector2(-1, 0)
-			if event.key == pygame.K_RIGHT and game.snake.direction != Vector2(-1, 0):
-				game.snake.direction = Vector2(1, 0)
+			if event.type == pygame.KEYDOWN:
+				if game.state == "STOPPED":
+					game.state = "RUNNING"
+				if event.key == pygame.K_UP and game.snake.direction != Vector2(0, 1):
+					game.snake.direction = Vector2(0, -1)
+				if event.key == pygame.K_DOWN and game.snake.direction != Vector2(0, -1):
+					game.snake.direction = Vector2(0, 1)
+				if event.key == pygame.K_LEFT and game.snake.direction != Vector2(1, 0):
+					game.snake.direction = Vector2(-1, 0)
+				if event.key == pygame.K_RIGHT and game.snake.direction != Vector2(-1, 0):
+					game.snake.direction = Vector2(1, 0)
 
-	#Drawing
-	screen.fill(GREEN)
-	pygame.draw.rect(screen, DARK_GREEN, 
-		(OFFSET-5, OFFSET-5, cell_size*number_of_cells+10, cell_size*number_of_cells+10), 5)
-	game.draw()
-	title_surface = title_font.render("Retro Snake", True, DARK_GREEN)
-	score_surface = score_font.render(str(game.score), True, DARK_GREEN)
-	screen.blit(title_surface, (OFFSET-5, 20))
-	screen.blit(score_surface, (OFFSET-5, OFFSET + cell_size*number_of_cells +10))
+		#Drawing
+		screen.fill(GREEN)
+		pygame.draw.rect(screen, DARK_GREEN, 
+			(OFFSET-5, OFFSET-5, cell_size*number_of_cells+10, cell_size*number_of_cells+10), 5)
+		game.draw()
+		title_surface = title_font.render("Retro Snake", True, DARK_GREEN)
+		score_surface = score_font.render(str(game.score), True, DARK_GREEN)
+		screen.blit(title_surface, (OFFSET-5, 20))
+		screen.blit(score_surface, (OFFSET-5, OFFSET + cell_size*number_of_cells +10))
 
-	pygame.display.update()
-	clock.tick(60)
+		pygame.display.update()
+		clock.tick(60)
+		await asyncio.sleep(0)
+
+asyncio.run(main())
